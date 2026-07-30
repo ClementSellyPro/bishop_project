97479f373e37ac26169d726407a697f30dff5c45
ClementSellyPro <clementselly.pro@gmail.com>
2026-07-30 08:54:02 +0400

    Reseaux component for the Home page

---
+++ app/components/page/Home/Reseaux.tsx
@@ -0,0 +1,35 @@
+import Container from "../../shared/Container";
+import Image from "next/image";
+
+export default function Reseaux() {
+  return (
+    <Container theme="dark">
+      <div className="lg:w-full py-10 px-14 w-7/12 relative">
+        <h2 className="kaisei-opti-regular text-6xl font-semibold text-center">
+          Retrouvez nous sur nos réseaux
+        </h2>
+        <div className="flex justify-end gap-20 mt-18 mr-10">
+          <a
+            href="https://www.facebook.com/people/Les-Fous-Sin-Dni/61583722233152/"
+            target="_blank"
+          >
+            <div className="w-68 h-87.5 border rounded-2xl bg-white/15 hover:bg-white/30"></div>
+          </a>
+          <a
+            href="https://www.instagram.com/lesfous.sindni_974"
+            target="_blank"
+          >
+            <div className="w-68 h-87.5 border rounded-2xl bg-white/15 hover:bg-white/30"></div>
+          </a>
+        </div>
+        <Image
+          src="/illustration.png"
+          alt="Réseaux"
+          width={750}
+          height={750}
+          className="absolute -left-70 top-5"
+        />
+      </div>
+    </Container>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
 import Header from "./components/Header";
 import Hero from "./components/page/Home/Hero";
 import Container from "./components/shared/Container";
+import Presentation from "./components/page/Home/Presentation";
+import Reseaux from "./components/page/Home/Reseaux";
 
 export default function Home() {
   return (
@@ -9,42 +10,9 @@ export default function Home() {
       <Header />
       <Hero />
 
-      <Container theme="light">
-        <div className="ml-20 mt-14 w-7/12 relative">
-          <h2 className="kaisei-opti-regular text-7xl font-semibold mb-12">
-            PRESENTATION
-          </h2>
-          <section className="flex flex-col gap-4 text-xl tracking-wide">
-            <p>
-              Nous proposons des cours d'initiation, de perfectionnement ouvert
-              à tous, enfants et adultes 🧒👧
-            </p>
-            <p>
-              Nous organisons aussi diverses animations : Tournois, Ateliers
-              d'initiation, Rencontres amicales etc. 🔥
-            </p>
-            <p>
-              Notre club est affilié à la Fédération Française des Échecs (FFE)
-              et à la Ligue Réunionnaise du Jeu d’Échecs (LRJE) 🤝 ♟️
-            </p>
-            <p>
-              Le club est une association loi 1901 qui a pour but de promouvoir
-              la pratique, ainsi que le développement du jeu d'échecs ♟️
-            </p>
-          </section>
-          <Image
-            src="/illustration_0.png"
-            alt="Présentation"
-            width={800}
-            height={800}
-            className="absolute -right-170 -top-30"
-          />
-        </div>
-      </Container>
+      <Presentation />
 
-      <Container theme="dark">
-        <p>TEST</p>
-      </Container>
+      <Reseaux />
 
       <Container theme="light">
         <p>TEST</p>
