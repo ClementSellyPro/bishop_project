c86efcbfc4a82b777c571749144fa77739a7d5d8
ClementSellyPro <clementselly.pro@gmail.com>
2026-07-31 17:18:13 +0400

    HeroAbout for About page

---
+++ app/about/components/HeroAbout.tsx
@@ -0,0 +1,23 @@
+export default function HeroAbout() {
+  return (
+    <div
+      className="min-h-screen flex justify-center items-center"
+      style={{
+        backgroundImage: "url('/about_bg.png')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="bg-gray-50/40 px-10 py-30 rounded-xl w-2/3 backdrop-blur-md">
+        <h1 className="kaisei-opti-regular text-7xl font-bold mb-4 text-center">
+          À PROPOS
+        </h1>
+        <p>
+          Le club a été créé en 2025 par quelques passionnés du jeu d'échecs.
+          L'objectif est de créer un espace bienveillant et de transmettre la
+          passion pour ce jeu millénaire !{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,14 @@
+import HeroAbout from "./components/HeroAbout";
 import Membres from "./components/Membre";
 import Partenaire from "./components/Partenaire";
 
 export default function About() {
   return (
     <div className="pt-28">
-      {/* 
-        <div className="px-4 py-6">
-          <h1 className="kaisei-opti-regular text-7xl font-bold mb-4">
-            À propos
-          </h1>
-          <h2 className="text-4xl font-semibold mb-4">Un peu d'histoire</h2>
-          <p>
-            Le club a été créé en 2025 par quelques passionnés du jeu d'échecs.
-            L'objectif est de créer un espace bienveillant et de transmettre la
-            passion pour ce jeu millénaire !{" "}
-          </p>
-        </div>
-      */}
+      <HeroAbout />
+
       <Partenaire />
+
       <Membres />
     </div>
   );
