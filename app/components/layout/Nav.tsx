d175898c4249eec35cb0fe2715a853aa52218705
ClementSellyPro <clementselly.pro@gmail.com>
2026-08-08 11:13:11 +0400

    Header responsive

--- app/components/layout/Nav.tsx
+++ app/components/layout/Nav.tsx
@@ -1,15 +1,62 @@
+"use client";
+
+import Image from "next/image";
 import NavItem from "./NavItem";
+import { useState, useEffect } from "react";
 
 export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", closeMenu);
+
+    return () => {
+      window.removeEventListener("scroll", closeMenu);
+    };
+  }, [isMenuOpen]);
+
   return (
-    <nav className="bg-transparent text-black p-4">
-      <ul className="flex h-10 px-4">
-        <NavItem href="/" label="Accueil" />
-        <NavItem href="/cours" label="Cours" />
-        <NavItem href="/evenements" label="Événements" />
-        <NavItem href="/contact" label="Contact" />
-        <NavItem href="/about" label="À propos" />
-      </ul>
-    </nav>
+    <>
+      <nav className="bg-transparent text-black p-4 relative">
+        <Image
+          src="/icon/hamburger.png"
+          alt="Bouton de menu"
+          width={70}
+          height={70}
+          className="w-10 lg:w-20 lg:hidden cursor-pointer"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+
+        <ul className="hidden lg:flex gap-4 lg:h-10 px-4 bg-transparent w-auto">
+          <NavItem href="/" label="Accueil" onClick={closeMenu} />
+          <NavItem href="/cours" label="Cours" onClick={closeMenu} />
+          <NavItem href="/evenements" label="Événements" onClick={closeMenu} />
+          <NavItem href="/contact" label="Contact" onClick={closeMenu} />
+          <NavItem href="/about" label="À propos" onClick={closeMenu} />
+        </ul>
+
+        {isMenuOpen && (
+          <ul
+            className="lg:hidden flex flex-col gap-4 px-4 pt-4 pb-8 
+          bg-white absolute top-18 -right-4 w-screen h-auto
+            shadow-lg z-50 rounded-xl"
+          >
+            <NavItem href="/" label="Accueil" onClick={closeMenu} />
+            <NavItem href="/cours" label="Cours" onClick={closeMenu} />
+            <NavItem
+              href="/evenements"
+              label="Événements"
+              onClick={closeMenu}
+            />
+            <NavItem href="/contact" label="Contact" onClick={closeMenu} />
+            <NavItem href="/about" label="À propos" onClick={closeMenu} />
+          </ul>
+        )}
+      </nav>
+    </>
   );
 }
--- app/components/layout/NavItem.tsx
+++ app/components/layout/NavItem.tsx
@@ -3,15 +3,19 @@ import Link from "next/link";
 export default function NavItem({
   href,
   label,
+  onClick,
 }: {
   href: string;
   label: string;
+  onClick: () => void;
 }) {
   return (
-    <li className="space-x-2">
+    <li className="space-x-2" onClick={onClick}>
       <Link
         href={href}
-        className="flex items-center text-lg lg:text-2xl px-6 hover:scale-110 transition-all duration-150 ease-in-out h-full"
+        className="flex items-center text-lg lg:text-2xl px-6 hover:scale-110 
+        transition-all duration-150 ease-in-out h-full
+        shadow-sm rounded-xl py-2 lg:py-0 lg:shadow-none"
       >
         {label}
       </Link>
