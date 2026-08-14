d3ed47361a37c8192363481d37f50f21cd29660f
ClementSellyPro <clementselly.pro@gmail.com>
2026-08-14 15:21:33 +0400

    object-cover for the background

--- app/components/home/Background.tsx
+++ app/components/home/Background.tsx
@@ -3,8 +3,14 @@ import Image from "next/image";
 export default function Background() {
   return (
     <div className="fixed inset-0 -z-10">
-      <Image src="/bg_0.jpg" alt="Background" fill sizes="100vw" />
-      <div className="absolute inset-0 bg-black/15 object-contain lg:object-fill"></div>
+      <Image
+        src="/bg_0.jpg"
+        alt="Background"
+        fill
+        sizes="100vw"
+        className=" object-cover lg:object-fill"
+      />
+      <div className="absolute inset-0 bg-black/15"></div>
     </div>
   );
 }
